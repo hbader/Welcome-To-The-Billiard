733ae8eedcb81651073750e769696ec86a80c41b
hbader <hunterbader16@augustana.edu>
2020-02-20 20:51:17 -0600

    Added Spawnpoints

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public RigidBody rb;
+
+    public float force = 2000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.getButtonDown("Right"))
+        {
+            rb.AddForce();
+        }
+    }
+}
