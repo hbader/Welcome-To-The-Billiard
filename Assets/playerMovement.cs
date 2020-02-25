b1847974ef4f7c335402b1338fc2e15a8ffeb32d
hbader <hunterbader16@augustana.edu>
2020-02-25 01:04:10 -0600

    Updated controls

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")); ;
+        rb.AddForce(movement*speed*Time.deltaTime);
+    }
+}
