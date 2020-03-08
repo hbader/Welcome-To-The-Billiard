33bfe28b0f543584d14356897a4b238a59476813
hbader <hunterbader16@augustana.edu>
2020-03-07 20:04:40 -0600

    Added Enemy Prefab and Enemy A.I.

---
+++ Assets/Scripts/batsController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batsController: MonoBehaviour
+{
+    public Rigidbody rb;
+    GameObject target;
+    float moveSpeed;
+    Vector3 directionToTarget;
+   // public GameObject explosion;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        moveSpeed = 200*Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        moveEnemy();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Player")) {
+            if (col.attachedRigidbody.velocity.magnitude > 5)
+            {
+               // Instantiate(explosion, transform.position, Quaternion.identity);
+                //add kill counter maybe?
+                Destroy(gameObject);
+            }
+            else
+            {
+                enemySpawnerControl.spawnAllowed = false;
+               // Instantiate(explosion, col.gameObject.transform.position, Quaternion.identity);
+                Destroy(col.gameObject);
+                target = null;
+            }
+        }
+    }
+
+    void moveEnemy()
+    {
+        if(target != null)
+        {
+            directionToTarget = (target.transform.position - transform.position).normalized;
+            rb.velocity = 
+                new Vector3(directionToTarget.x * moveSpeed,
+                0,
+                directionToTarget.z * moveSpeed);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
+
---
+++ Assets/Scripts/enemySpawnerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawnerControl : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] monsters;
+
+    int randomSpawnpoint, randomMonster;
+    public static bool spawnAllowed;
+
+    private void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("spawnAnEnemy", 0f, 1f);
+    }
+
+    private void spawnAnEnemy()
+    {
+        if (spawnAllowed)
+        {
+            randomSpawnpoint = Random.Range(0, spawnPoints.Length);
+            randomMonster = Random.Range(0, monsters.Length);
+            Instantiate(monsters[randomMonster], spawnPoints[randomSpawnpoint].position, Quaternion.identity);
+        }
+    }
+}
+
