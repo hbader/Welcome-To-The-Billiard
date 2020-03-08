956ee07b016aa6e3d6889473196ef464eee4195f
hbader <hunterbader16@augustana.edu>
2020-03-07 21:46:05 -0600

    Working Speed Bar Prototype

---
+++ Assets/Scripts/SpeedBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+
+    public Image fill;
+
+    public void setMaxSpeed(float speed)
+    {
+        slider.maxValue = speed;
+    }
+
+    public void setSpeed(float speed)
+    {
+        slider.value = speed;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float speed;
+    private float representedVelocity;
     private Rigidbody rb;
 
+    public SpeedBar speedBar;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        speedBar.setMaxSpeed(10f);
     }
 
     void FixedUpdate()
@@ -21,6 +24,13 @@ public class playerMovement : MonoBehaviour
         Vector3 movement = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * inputV;
         if (rb.velocity.magnitude > 0.1f || vert >= 0) { 
             rb.AddForce(movement * speed * Time.deltaTime);
-        } 
+        }
+        updateSpeed(rb.velocity.magnitude);
     }
-}
+
+    private void updateSpeed(float speed)
+    {
+        representedVelocity = speed;
+        speedBar.setSpeed(representedVelocity);
+    }
+    }
